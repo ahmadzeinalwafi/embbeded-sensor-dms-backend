ed6f89153ae8cc5e84eb5d40db37e1b998561bdb
Ahmad Zein Al Wafi <drakeequehours@gmail.com>
2025-02-02 02:04:10 +0700

    mod: using env for influxdb key

--- internal/infrastructure/persistance/influxdb/influxdb.go
+++ internal/infrastructure/persistance/influxdb/influxdb.go
@@ -2,14 +2,15 @@ package influxdb
 
 import (
 	"context"
-"dms/config"
+	"dms/config"
+
 	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
 )
 
 func InfluxDBConnector() (influxdb2.Client, error) {
 	cfg := config.LoadConfig()
 
-	client := influxdb2.NewClient(cfg.GetString("INFLUXDB_URL"), "QfAga1dLkl5nciEq8kmA1wqNImfi48FYNpncgYic7LSg1a-JyeR-2pmIwy_8_u_k06b2x5RmcdyzOy3H_3FSog==")
+	client := influxdb2.NewClient(cfg.GetString("INFLUXDB_URL"), cfg.GetString("INFLUXDB_KEY"))
 
 	// validate client connection health
 	_, err := client.Health(context.Background())
